68d73c50e2a40621efbccf3be626832de8530555
yutocreate <1otuy26@gmail.com>
2022-10-10 20:33:46 +0900

    coins

--- opt/coins.py
+++ opt/coins.py
@@ -1,13 +1,2 @@
-a = input()
-b = input()
-c = input()
-x = input()
-count = 0
-
-for i in range(a+1):
-  for j in range(b+1):
-    for k in range(c+1):
-      if i*500 + j*100 + k*50 == x:
-        count+=1
-
-print(count)
+a,b,c,x=map(int,open(0))
+print(sum(500*i+100*j+50*k==x for i in range(a+1)for j in range(b+1)for k in range(c+1)))
